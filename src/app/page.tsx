a0e84171098a44b77ae2f2e45cb4a821ea8bfada
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-30 11:18:43 +0300

    Added the Overlay component to the page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import CanvasComponent from '@/components/canvas'
+import Overlay from '@/components/overlay'
 
 const Home = () => {
 	return (
 		<main className='h-screen'>
 			<CanvasComponent position={[-1, 0, 2.5]} fov={25} />
+			<Overlay />
 		</main>
 	)
 }
