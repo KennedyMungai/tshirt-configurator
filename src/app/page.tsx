041fca3a71f6fbb51ca05754d55186882954328d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-30 09:07:49 +0300

    Reset the styling for the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import CanvasComponent from '@/components/canvas'
 
 const Home = () => {
 	return (
-		<main>
+		<main className='h-screen'>
 			<CanvasComponent position={[-1, 0, 2.5]} fov={25} />
 		</main>
 	)
