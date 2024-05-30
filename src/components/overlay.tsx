df1a9e00a28d4e6d6d87fa2c2e109938b3d36803
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-30 14:22:32 +0300

    Added some markup and styling to the page

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -1,11 +1,46 @@
 import { Logo } from '@pmndrs/branding'
+import { AiOutlineHighlight, AiOutlineShopping } from 'react-icons/ai'
 
 const Overlay = () => {
 	return (
-		<div className='z-10 container'>
-			<header className='absolute top-0 left-0 text-orange-500 text-4xl font-semibold'>
+		<div className='z-10 container absolute top-0 left-0 h-full w-full'>
+			<header className='flex justify-between p-10 text-white'>
 				<Logo width={40} height={40} />
+				<AiOutlineShopping size='3rem' />
 			</header>
+
+			<section key='main'>
+				<div className='flex flex-col'>
+					<div className='w-60 pl-10'>
+						<h1 className='text-9xl italic uppercase font-bold text-[#f85]'>
+							LET&apos;S DO IT
+						</h1>
+					</div>
+					<div className='absolute right-2 top-60'>
+						<div className='w-72 flex flex-col gap-20'>
+							<p className='text-2xl text-orange-400'>
+								Create your unique and exclusive shirt with our
+								brand-new customization tool.{' '}
+								<strong>Unleash your imagination</strong> and
+								define your own style
+							</p>
+							<button
+								style={{
+									background: 'black',
+									color: 'white',
+									borderRadius: 6,
+									padding: 10,
+									display: 'flex',
+									gap: 5
+								}}
+							>
+								CUSTOMIZE IT{' '}
+								<AiOutlineHighlight size='1.3rem' />
+							</button>
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
 	)
 }
