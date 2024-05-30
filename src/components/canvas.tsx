af6ca243b96cf8c5c8ca1aa367a6d5e9cb4d7916
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-30 10:16:31 +0300

    Added some props to the RandomizedLights component

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -38,7 +38,13 @@ const Backdrop = () => (
 		rotation={[Math.PI / 2, 0, 0]}
 		position={[0, 0, -0.14]}
 	>
-		<RandomizedLight />
+		<RandomizedLight
+			amount={4}
+			radius={9}
+			intensity={0.75}
+			ambient={0.25}
+			position={[5, 5, -10]}
+		/>
 	</AccumulativeShadows>
 )
 
