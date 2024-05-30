4fe51a64b29b48a9324b21f0b132ed54f3ebab06
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-30 09:53:49 +0300

    Added the tshirt model and some ambient lighting for the scene

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Center, OrbitControls } from '@react-three/drei'
+import { Center, OrbitControls, useGLTF } from '@react-three/drei'
 import { Canvas as ThreeCanvas } from '@react-three/fiber'
 
 type Props = {
@@ -7,18 +7,27 @@ type Props = {
 	fov: number
 }
 
-const Shirt = () => (
-	<mesh>
-		<boxGeometry args={[0.9, 0.9, 0.9]} />
-		<meshNormalMaterial />
-	</mesh>
-)
+const TShirt = (props) => {
+	const { nodes, materials } = useGLTF('/t-shirt.glb')
+	return (
+		<group {...props} dispose={null}>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.T_Shirt_male.geometry}
+				material={materials.lambert1}
+				rotation={[Math.PI / 2, 0, 0]}
+			/>
+		</group>
+	)
+}
 
 const CanvasComponent = ({ fov = 25, position = [-1, 0, 2.5] }: Props) => {
 	return (
 		<ThreeCanvas camera={{ position, fov }} className='h-full'>
+			<ambientLight intensity={0.75} />
 			<Center>
-				<Shirt />
+				<TShirt />
 			</Center>
 
 			<OrbitControls />
