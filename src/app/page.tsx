bc6a07d80fbd466fbbb110a365b642ac3eaa6864
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-30 08:13:57 +0300

    Added some props for the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CanvasComponent from '@/components/canvas'
 const Home = () => {
 	return (
 		<main>
-			<CanvasComponent />
+			<CanvasComponent position={[-1, 0, 2.5]} fov={25} />
 		</main>
 	)
 }
