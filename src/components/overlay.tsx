ea14901649945c7373b7afd8ea1ddd4cbe9fb3bb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-30 12:33:05 +0300

    Added the Logo component to the header component

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -1,8 +1,10 @@
+import { Logo } from '@pmndrs/branding'
+
 const Overlay = () => {
 	return (
 		<div className='z-10 container'>
 			<header className='absolute top-0 left-0 text-orange-500 text-4xl font-semibold'>
-				T-Shirt Configurator
+				<Logo width={40} height={40} />
 			</header>
 		</div>
 	)
