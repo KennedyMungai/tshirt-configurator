ee95cb563f9d2d5b61003535e67f20232cfad9f1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-30 12:28:42 +0300

    Added some classes for the header

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -1,5 +1,11 @@
 const Overlay = () => {
-	return <h1>Hello</h1>
+	return (
+		<div className='z-10 container'>
+			<header className='absolute top-0 left-0 text-orange-500 text-4xl font-semibold'>
+				T-Shirt Configurator
+			</header>
+		</div>
+	)
 }
 
 export default Overlay
